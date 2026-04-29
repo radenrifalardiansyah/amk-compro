df961a3cfdd3de2c671cb2cc595c592a07319525
Raden Rifal Ardiansyah <radenrifalardiansyah@gmail.com>
2026-04-29 08:47:28 +0700

    Update shortcut to open the input folder in the terminal:

--- src/js/main.js
+++ src/js/main.js
@@ -172,11 +172,11 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Mencegah Klik Kanan
-// document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
 
-// // Mencegah shortcut keyboard tertentu
-// document.onkeydown = function (e) {
-//     if (e.keyCode == 123) return false; // F12
-//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false; // Inspect
-//     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false; // View Source
-// };
+// Mencegah shortcut keyboard tertentu
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) return false; // F12
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false; // Inspect
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false; // View Source
+};
